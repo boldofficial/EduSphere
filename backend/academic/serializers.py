63dbe42b0952f7d556d669eaf96350555c435fc1
Sholems <sholemsreal@gmail.com>
2026-02-13 22:50:30 +0100

    Fix: Reorder serializers to prevent NameError in backend

--- backend/academic/serializers.py
+++ backend/academic/serializers.py
@@ -429,18 +429,18 @@ class ReportCardSerializer(serializers.ModelSerializer):
             
         report_card.update_totals()
 
+class AttendanceRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AttendanceRecord
+        fields = '__all__'
+        read_only_fields = ('school',)
+
 class AttendanceSessionSerializer(serializers.ModelSerializer):
     records = AttendanceRecordSerializer(many=True, read_only=True)
     class_name = serializers.CharField(source='student_class.name', read_only=True)
     
     class Meta:
         model = AttendanceSession
-        fields = '__all__'
-        read_only_fields = ('school',)
-
-class AttendanceRecordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AttendanceRecord
         fields = '__all__'
         read_only_fields = ('school',)
 
