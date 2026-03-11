61445d5b48c968c17694d5d3572050c230caeea2
zieeco <zieecosam@gmail.com>
2026-03-11 07:34:47 +0100

    refactor: add the email marketting

---
+++ app/dashboard/super-admin/marketing/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import { EmailMarketingTab } from '@/components/features/dashboard/EmailMarketingTab';
+
+export default function MarketingPage() {
+    return <EmailMarketingTab />;
+}
