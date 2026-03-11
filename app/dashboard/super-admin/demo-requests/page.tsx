82443c67cb3d9458924c6b2aa78115f831e5def3
zieeco <zieecosam@gmail.com>
2026-03-11 07:39:06 +0100

    refactor: add the demo-requests page

---
+++ app/dashboard/super-admin/demo-requests/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import { DashboardDemoRequestsTab } from '@/components/features/dashboard/DashboardDemoRequestsTab';
+
+export default function DemoRequestsPage() {
+    return <DashboardDemoRequestsTab />;
+}
